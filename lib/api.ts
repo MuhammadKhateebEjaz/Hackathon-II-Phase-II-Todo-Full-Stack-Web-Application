359cd5f9d5492da21bb502e96643ff5e7c5cfc00
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2026-01-08 01:48:25 +0500

    Delete lib directory

--- lib/api.ts
+++
@@ -1,9 +0,0 @@
-// Starter API client
-export const api = {
-  getTasks: async () => {
-    return await fetch("/api/tasks").then(res => res.json());
-  },
-  createTask: async (data: any) => {
-    return await fetch("/api/tasks", { method: "POST", body: JSON.stringify(data) });
-  }
-}
